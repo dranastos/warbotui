2b18bd8eb9e2f79b3bf2bfb8f8436c55d603a2a2
dranastos <info@chainwire.io>
2021-07-08 14:57:02 -0400

    added buttons to warbot interface

--- forms/WarbotInventory.js
+++ forms/WarbotInventory.js
@@ -21,6 +21,8 @@ const WarbotInventory = ({ onComplete, address }) => {
   const [total, setTotal] = useState(0)
   const [loading, setLoading] = useState(false)
   const [hasVaults, setHasVaults] = useState(false)
+  const [warbot1, setWarbot1] = useState(false)
+
 
   useEffect(() => {
     if (connected && state.hasWarbotmanufacturer) {
@@ -66,7 +68,12 @@ const WarbotInventory = ({ onComplete, address }) => {
     setHasVaults(true)
     setLoading(false)
   }
-
+  
+   const updateWarbotMain = async(e) => {
+   try{
+    setWarbot1 ( 15 )
+   } catch (e) {}
+  }
   
  
 
@@ -86,7 +93,12 @@ const WarbotInventory = ({ onComplete, address }) => {
           <Collapse.Panel header={`${warbotid} -${warbotlevel}`}>
             <Row style={{ marginTop: 10 }} gutter={[20, 20]}>
               <Col span={24}>
-                
+               <Space>
+					<Button size="large" type="primary" >Upgrade</Button>
+					<Button size="large" style={{ background: "red", borderColor: "yellow" }} >Send For Parts</Button>
+					<Button size="large" style={{ background: "yellow", borderColor: "yellow" }} >Send To Warbot Dealership</Button>
+					<Button size="large" type="danger" >Send To The Warzone</Button>
+				</Space>  
                   
               </Col>
               <Col span={12}>
