d2f443f3757bbf418c23561bb3279b6b8b864d84
Aleksandr Eleskin <eleskin1201@gmail.com>
2021-12-13 23:41:23 +0300

    feat: add interaction with contract to dashboard form

--- components/manufacturing/Dashboard/Dashboard.js
+++ components/manufacturing/Dashboard/Dashboard.js
@@ -4,10 +4,135 @@ import Card from '../../Card/Card';
 import Input from '../../Input/Input';
 import Button from '../../Button/Button';
 import Range from '../../Range/Range';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
+import useMicroMachines from '../../../hooks/useMicroMachines';
+import useGlobal from '../../../hooks/useGlobal';
+import {useWallet} from 'use-wallet';
+import useMicroMachineManufacturingPlant from '../../../hooks/useMicroMachineManufacturingPlant';
+import {notification} from 'antd';
 
 const Dashboard = () => {
+	const wallet = useWallet();
 	const [periods, setPeriods] = useState(1);
+	const [balance, setBalance] = useState(0);
+	const [counter, setCounter] = useState(0);
+	const [allowance, setAllowance] = useState(0);
+	const [state, actions] = useGlobal([
+		'warbotmanufacturer',
+		'hasWarbotmanufacturer',
+		'micromachines',
+		'hasMicromachines',
+	]);
+	const [micromachines] = useMicroMachines(state.micromachines);
+	const [sufficientlyApproved, setSufficientlyApproved] = useState(0);
+	const [insufficientlyApproved, setInsufficientlyApproved] = useState(0);
+	const {warbotmanufacturer, web3, connected} = useMicroMachineManufacturingPlant(state.warbotmanufacturer);
+	const [data, setData] = useState({months: 0, amount: 0});
+
+	const getBalance = async () => {
+		const balance = await micromachines.balanceOf(wallet.account).call();
+		setBalance(web3.utils.fromWei(balance, 'nano'));
+		setCounter(counter + 1);
+	};
+
+	const getAllowance = async () => {
+		const balance = await micromachines.allowance(wallet.account, state.warbotmanufacturer).call();
+		setAllowance(web3.utils.fromWei(balance, 'nano'));
+		setSufficientlyApproved('stake_btn2');
+		setInsufficientlyApproved('stake_btn');
+		setCounter(counter + 1);
+	};
+
+	const approve = async () => {
+		try {
+			if (data.amount > 0) {
+				const value = web3.utils
+					.toWei(((data.amount || 0) * 1.0).toString(), 'nano')
+					.toString();
+
+				console.log('APPROVAL AMOUNT', value);
+
+				const tx = await micromachines
+					.approve(state.warbotmanufacturer, value)
+					.send({
+						from: wallet.account,
+						to: state.warbotmanufacturer,
+					});
+
+				if (tx.status) {
+					notification.success({
+						message: 'Approve Successful',
+						description: tx.transactionHash,
+					});
+
+					await getAllowance();
+				}
+			}
+		} catch (e) {
+			notification.error({
+				message: 'Approval Failed',
+				description: e.toString(),
+			});
+		}
+	};
+
+	const handleDeposit = async () => {
+		setLoading(true);
+
+		try {
+			const value = web3.utils
+				.toWei(data.amount.toString(), 'nano')
+				.toString();
+
+			console.log('STAKE MICROMACHINES ', value, parseInt(data.months));
+
+			const tx = await warbotmanufacturer
+				.stakeMicroMachines(value, parseInt(data.months), [0, 0, 0])
+				.send({from: wallet.account, to: state.warbotmanufacturer});
+
+			if (tx.status) {
+				setData({amount: 0, months: 0});
+				notification.success({
+					message: 'Deposit Successful',
+					description: tx.transactionHash,
+				});
+
+				actions.addVaultCount();
+			}
+		} catch (e) {
+			notification.error({
+				message: 'Deposit Failed',
+				description: e.toString(),
+			});
+		}
+
+		setLoading(false);
+	};
+
+	const notEnough = () => {
+	};
+
+	const getTimeDeposit = async () => {
+		const weiValue = web3.utils
+			.toWei((data.amount || 0).toString(), 'nano')
+			.toString();
+		//const bonus = await security.timeValueDepositAmount(weiValue, parseInt(data.months)).call()
+
+		console.log('VALUE', weiValue);
+	};
+
+	useEffect(() => {
+		if (connected && state.hasWarbotmanufacturer) {
+			getTimeDeposit();
+		}
+	}, [data, periods]);
+
+	useEffect(() => {
+		if (connected && micromachines && state.hasMicromachines) {
+			getBalance();
+			getAllowance();
+		}
+	}, [connected, micromachines, state.hasMicromachines]);
 
 	return (
 		<div className={styles.Dashboard}>
@@ -22,40 +147,47 @@ const Dashboard = () => {
 					<Card.Header
 						title="Stake your MMAC"
 						subtitle="1 MMAC = 1 Warbot every 90 days"
-						outputValue="3 MMAC"
+						outputValue={`${balance} MMAC`}
 					/>
 					<Card.Main>
 						<div className={styles.Dashboard__label}>
 							<Input
 								label="Deposit amount"
 								placeholder="E.G. 10"
+								value={data.amount}
+								onInput={(event) => setData({...data, amount: event.target.value})}
 							/>
 						</div>
 						<div className={styles.Dashboard__label}>
 							<span>Time lock</span>
 							<Range
 								min={1}
-								max={10}
+								max={12}
 								step={1}
 								rightInputValue={periods}
 								setRightInputValue={setPeriods}
-								setMaxValue={() => {}}
+								setMaxValue={() => {
+								}}
 								isTwoThumbs={false}
 							/>
-							<p><b>{periods} periods</b> ({((periods - 1) / (3 * 0.8)).toFixed(1)} years)</p>
+							<p><b>{periods} periods</b> ({(periods / 4).toFixed(1)} years)</p>
 						</div>
 						<Button
 							value="Stake MMAC"
-							disabled={true}
+							disabled={!(allowance >= Number(data.amount) && Number(data.amount) * Number(periods) <= 50 && Number(data.amount) > 0)}
+							onClick={allowance >= Number(data.amount) ? handleDeposit : notEnough}
 						/>
 						<div className={styles.Dashboard__text}>
 							<p>
-								Build <i>369 Warbots</i> a period by locking your MicroMachines for 3 months for a total of <i>1107
-								Warbots</i>.
+								Build <i>{periods * Number(data.amount)} Warbots</i> a period by locking your MicroMachines
+								for {periods} months for a total of <i>{periods**2 * Number(data.amount)}&nbsp;Warbots</i>.
 							</p>
 						</div>
 					</Card.Main>
-					<Card.Footer/>
+					<Card.Footer
+						callback={approve}
+						disabled={allowance >= Number(data.amount) && Number(data.amount) * Number(periods) <= 50 && Number(data.amount) > 0}
+					/>
 				</Card>
 			</div>
 		</div>
--- forms/MicroMachineStakingForm.js
+++ forms/MicroMachineStakingForm.js
@@ -1,415 +1,56 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from 'react';
 import {
-    Typography,
-    Space,
-    Row,
-    Col,
-    Card,
-    Statistic,
-    Slider,
-    Form,
-    Spin,
-    Button,
-    Input,
-    notification,
-} from "antd";
-const { Title, Text } = Typography;
-import { useWallet } from "use-wallet";
-import useWeb3 from "../hooks/useWeb3";
-import useMicroMachineManufacturingPlant from "../hooks/useMicroMachineManufacturingPlant";
-import useGlobal from "../hooks/useGlobal";
+	Typography,
+	Space,
+	Row,
+	Col,
+	Card,
+	Statistic,
+	Slider,
+	Form,
+	Spin,
+	Button,
+	Input,
+	notification,
+} from 'antd';
 
-import useMicroMachines from "../hooks/useMicroMachines";
-import Manufacturing_plants from "../components/Manufacturing_plants";
-import UserManufacturingCenters from "./UserManufacturingCenters";
+const {Title, Text} = Typography;
+import {useWallet} from 'use-wallet';
+import useWeb3 from '../hooks/useWeb3';
+import useMicroMachineManufacturingPlant from '../hooks/useMicroMachineManufacturingPlant';
+import useGlobal from '../hooks/useGlobal';
+
+import useMicroMachines from '../hooks/useMicroMachines';
+import Manufacturing_plants from '../components/Manufacturing_plants';
+import UserManufacturingCenters from './UserManufacturingCenters';
 
 const slider_style = {
-    backgroundColor: " rgba(113, 238, 255, 0.7)",
-    webkitAppearance: "none",
-    appearance: "none",
-    height: "14px",
-    width: "100%",
-    borderRadius: "7px",
+	backgroundColor: ' rgba(113, 238, 255, 0.7)',
+	webkitAppearance: 'none',
+	appearance: 'none',
+	height: '14px',
+	width: '100%',
+	borderRadius: '7px',
 };
 const handle_style = {
-    backgroundColor: "#71eeff",
-    width: "30px",
-    height: "30px",
-    top: "0",
+	backgroundColor: '#71eeff',
+	width: '30px',
+	height: '30px',
+	top: '0',
 };
 
-const MicroMachineStakingForm = ({ onComplete, address }) => {
-    const wallet = useWallet();
-    const [state, actions] = useGlobal([
-        "warbotmanufacturer",
-        "hasWarbotmanufacturer",
-        "micromachines",
-        "hasMicromachines",
-    ]);
-    const { warbotmanufacturer, web3, connected } =
-        useMicroMachineManufacturingPlant(state.warbotmanufacturer);
-    // const [welfare] = useWelfare(state.welfare);
-    const [micromachines] = useMicroMachines(state.micromachines);
-    const [balance, setBalance] = useState(0);
-    const [allowance, setAllowance] = useState(0);
-    const [timeValue, setTimeValue] = useState(0);
-    const [canDeposit, setCanDeposit] = useState(false);
-    const [data, setData] = useState({ months: 0, amount: 0, timelock: 0 });
-    const [loading, setLoading] = useState(false);
-    const [counter, setCounter] = useState(0);
-    const [sufficientlyApproved, setSufficientlyApproved] = useState(0);
-    const [insufficientlyApproved, setInsufficientlyApproved] = useState(0);
+const MicroMachineStakingForm = ({onComplete, address}) => {
+	const [timeValue, setTimeValue] = useState(0);
+	const [canDeposit, setCanDeposit] = useState(false);
+	const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        if (connected && state.hasWarbotmanufacturer) {
-            getTimeDeposit();
-        }
-    }, [data]);
 
-    useEffect(() => {
-        if (connected && micromachines && state.hasMicromachines) {
-            getBalance();
-            getAllowance();
-        }
-    }, [connected, micromachines, state.hasMicromachines]);
 
-    const getBalance = async () => {
-        const balance = await micromachines.balanceOf(wallet.account).call();
-        setBalance(web3.utils.fromWei(balance, "nano"));
-        setCounter(counter + 1);
-    };
-
-    const getAllowance = async () => {
-        const balance = await micromachines
-            .allowance(wallet.account, state.warbotmanufacturer)
-            .call();
-        setAllowance(web3.utils.fromWei(balance, "nano"));
-        setSufficientlyApproved("stake_btn2");
-        setInsufficientlyApproved("stake_btn");
-        setCounter(counter + 1);
-    };
-
-    const getTimeDeposit = async () => {
-        const weiValue = web3.utils
-            .toWei((data.amount || 0).toString(), "nano")
-            .toString();
-        //const bonus = await security.timeValueDepositAmount(weiValue, parseInt(data.months)).call()
-
-        console.log("VALUE", weiValue);
-    };
-
-    const approve = async () => {
-        setLoading(true);
-
-        try {
-            if (data.amount > 0) {
-                const value = web3.utils
-                    .toWei(((data.amount || 0) * 1.0).toString(), "nano")
-                    .toString();
-
-                console.log("APPROVAL AMOUNT", value);
-
-                const tx = await micromachines
-                    .approve(state.warbotmanufacturer, value)
-                    .send({
-                        from: wallet.account,
-                        to: state.warbotmanufacturer,
-                    });
-
-                if (tx.status) {
-                    notification.success({
-                        message: "Approve Successful",
-                        description: tx.transactionHash,
-                    });
-
-                    await getAllowance();
-                }
-            }
-        } catch (e) {
-            notification.error({
-                message: "Approval Failed",
-                description: e.toString(),
-            });
-        }
-
-        setLoading(false);
-    };
-
-    const handleDeposit = async () => {
-        setLoading(true);
-
-        try {
-            const value = web3.utils
-                .toWei(data.amount.toString(), "nano")
-                .toString();
-
-            console.log("STAKE MICROMACHINES ", value, parseInt(data.months));
-
-            const tx = await warbotmanufacturer
-                .stakeMicroMachines(value, parseInt(data.months), [0,0,0])
-                .send({ from: wallet.account, to: state.warbotmanufacturer });
-
-            if (tx.status) {
-                setData({ amount: 0, months: 0 });
-                notification.success({
-                    message: "Deposit Successful",
-                    description: tx.transactionHash,
-                });
-
-                actions.addVaultCount();
-            }
-        } catch (e) {
-            notification.error({
-                message: "Deposit Failed",
-                description: e.toString(),
-            });
-        }
-
-        setLoading(false);
-    };
-
-    const handleTimeLock = (months, amount) => {
-        setData({ ...data, months });
-    };
-    const handleAmount = (e) => {
-        setData({ ...data, amount: parseInt(e.target.value) });
-    };
-
-    const notEnough = (e) => {};
-
-    return (
-        <Spin spinning={loading}>
-            <section className="For_innovators_tab">
-                <div className="container-lg">
-                    <div className="row">
-                        <div className="col-lg-5">
-                            <h1>Build a Warbot</h1>
-                            <p>
-                                Manufacture warbots by staking MMAC. The length of time you stake your MMAC for will dictate how many Warbots you manufacture. For every period you stake your MMAC, you manufacture 1 Warbot per period. A period is 90 days. Ex. Stake your MMAC for 1 period and you earn 1 warbot in 90 days Stake your MMAC for 4 periods and you earn 4 warbots every 90 days for 4 periods. Stake your MMAC for 12 periods and you earn 12 warbots every 90 days for 12 periods. At the end of the staking period, your MMAC will be returned to you.
-                            </p>
-                            {/* <Tank_model /> */}
-                        </div>
-                        <div className="col-lg-7">
-                            <div className="stake_box">
-                                <div className="stake_head">
-                                    <div className="stake_text">
-                                        <h3>Stake your MMAC</h3>
-                                        <p className="m-0">
-                                            1 MMAC = 1 Warbot every 90 days
-                                        </p>
-                                    </div>
-                                    <button className="mmac_btn">
-                                        {balance} MMAC
-                                    </button>
-                                </div>
-                                <hr />
-                                <div className="stake_input_div">
-                                    {/* <label>Deposit amount</label> */}
-                                    <Form.Item
-                                        name="vAmount"
-                                        label="Deposit Amount"
-                                        rules={[
-                                            {
-                                                required: true,
-                                                message: "Enter deposit amount",
-                                            },
-                                        ]}
-                                    >
-                                        <Input
-                                            type="number"
-                                            placeholder="123"
-                                            value={data.amount}
-                                            onChange={handleAmount}
-                                        />
-                                    </Form.Item>
-                                    {/* <input
-                                        type="text"
-                                        placeholder="123"
-                                        value={data.amount}
-                                        onChange={handleAmount}
-                                    /> */}
-                                    <label>Time lock :</label>
-
-                                    <Form.Item
-                                    // name="vTimelock"
-                                    // label="Time Lock"
-                                    // name="timelock"
-                                    // id="slider"
-                                    >
-                                        <Slider
-                                            min={1}
-                                            max={12}
-                                            defaultValue={3}
-                                            value={data.months}
-                                            onChange={handleTimeLock}
-                                            trackStyle={slider_style}
-                                            handleStyle={handle_style}
-                                        />
-                                        <Space>
-                                            <p className="time_line">
-                                                <b>{data.months} periods </b>
-                                                <span>
-                                                    {" "}
-                                                    ({" "}
-                                                    {parseFloat(
-                                                        parseInt(data.months) /
-                                                            4
-                                                    ).toFixed(1)}{" "}
-                                                    years)
-                                                </span>
-                                            </p>
-                                        </Space>
-                                    </Form.Item>
-                                    {/* <input
-                                        type="range"
-                                        min="1"
-                                        max="12"
-                                        id="slider"
-                                    />
-
-                                    <p className="time_line">
-                                        <b>3 quarters </b>
-                                        <span> (0.8 years)</span>
-                                    </p> */}
-                                    <button
-                                        className="stake_btn2"
-                                        onClick={approve}
-                                    >
-                                        Approve Contract
-                                    </button>
-									<button
-                                        className={
-                                            allowance >= data.amount &&
-                                            data.amount * data.months <= 50
-                                                ? sufficientlyApproved
-                                                : insufficientlyApproved
-                                        }
-                                        // className="stake_btn"
-                                        // type={
-                                        //     allowance >= data.amount &&
-                                        //     data.amount * data.months <= 50
-                                        //         ? sufficientlyApproved
-                                        //         : insufficientlyApproved
-                                        // }
-                                        onClick={
-                                            allowance >= data.amount
-                                                ? handleDeposit
-                                                : notEnough
-                                        }
-                                    >
-                                        Stake MMAC
-                                    </button>
-                                    <p className="stake_info">
-                                        Build{" "}
-                                        <span>
-                                            {data.amount * data.months} Warbots{" "}
-                                        </span>
-                                        a period by locking your MicroMachines
-                                        for {data.months} periods for a total of
-                                        <span>
-                                            {" "}
-                                            {data.amount *
-                                                data.months *
-                                                data.months}{" "}
-                                            Warbots
-                                        </span>
-                                        .
-                                    </p>
-                                </div>
-                                <hr />
-                                <div className="stake_input_div">
-                                    
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <UserManufacturingCenters />
-            {/* <Card title="Micromachine Staking">
-                <Card title="Build WarBot Manufacturing Center">
-                    <Form size="large" layout="vertical">
-                        <Statistic title="Balance" value={balance} />
-                        <Statistic title="Approved" value={allowance} />
-                        <Form.Item
-                            name="vAmount"
-                            label="Deposit Amount"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: "Enter deposit amount",
-                                },
-                            ]}
-                        >
-                            <Input
-                                type="number"
-                                placeholder="e.g 10000"
-                                value={data.amount}
-                                onChange={handleAmount}
-                            />
-                        </Form.Item>
-                        <Form.Item
-                            name="vTimelock"
-                            label="Time Lock"
-                            name="timelock"
-                            id="slider"
-                        >
-                            <Slider
-                                min={1}
-                                max={12}
-                                defaultValue={1}
-                                value={data.months}
-                                onChange={handleTimeLock}
-                            />
-                            <Space>
-                                <Text>{data.months} Quarter(s)</Text>
-                                <Text>
-                                    {parseFloat(
-                                        parseInt(data.months) / 4
-                                    ).toFixed(1)}{" "}
-                                    Year(s)
-                                </Text>
-                            </Space>
-                        </Form.Item>
-                        <Space>
-                            <Button
-                                size="large"
-                                type="primary"
-                                onClick={approve}
-                            >
-                                Approve
-                            </Button>
-                            <Button
-                                size="large"
-                                type={
-                                    allowance >= data.amount &&
-                                    data.amount * data.months <= 50
-                                        ? sufficientlyApproved
-                                        : insufficientlyApproved
-                                }
-                                onClick={
-                                    allowance >= data.amount
-                                        ? handleDeposit
-                                        : notEnough
-                                }
-                            >
-                                Stake MicroMachines
-                            </Button>
-                        </Space>
-                        <Card style={{ marginTop: 20, textAlign: "center" }}>
-                            <Text level={5} strong>
-                                Build {data.amount * data.months} WarBots a
-                                period by locking your MicroMachines for{" "}
-                                {data.months} period(s) for a total of{" "}
-                                {data.amount * data.months * data.months}{" "}
-                                WarBots
-                            </Text>
-                        </Card>
-                    </Form>
-                </Card>
-            </Card> */}
-        </Spin>
-    );
+	return (
+		<Spin spinning={loading}>
+			<UserManufacturingCenters/>
+		</Spin>
+	);
 };
 // export const mmac_bal = { MMAC_balance };
 export default MicroMachineStakingForm;
